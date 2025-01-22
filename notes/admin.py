a7e2664f76339530738fbf6f4568261d3044d1ae
Vinnicius Castro <90991299+vinniciuscastro@users.noreply.github.com>
2025-01-22 12:32:36 -0700

    test

--- notes/admin.py
+++ notes/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from . import models
 from .models import Note
 # Register your models here.
+
 class NoteAdmin(admin.ModelAdmin):
     list_display = ('pk','title', 'likes')
     list_display_links = ('title',)
